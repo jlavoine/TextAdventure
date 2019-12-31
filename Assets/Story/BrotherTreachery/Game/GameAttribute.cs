d2f43b5df94b49a4be12f73f69e532f36eacbda3
jlavoine <joe.lavoine@gmail.com>
2019-12-31 13:28:52 -0500

    First commit
    Project settings, some random junk, and game attribute enum.

---
+++ Assets/Story/BrotherTreachery/Game/GameAttribute.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Story.BrotherTreachery {
+    public enum GameAttribute {
+        Family,
+        Food,
+        War,
+        Peace,
+        Patriotism,
+        Religion,
+        Bribery
+    }
+}
